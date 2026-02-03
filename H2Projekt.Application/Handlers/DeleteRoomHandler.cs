9fb5e657595a271aa8f583f3f83aae16876cb8ea
mikk922r <mikk922r@edu.mercantec.dk>
2026-02-03 10:11:19 +0100

    Added a button for deleting rooms

---
+++ H2Projekt.Application/Handlers/DeleteRoomHandler.cs
@@ -0,0 +1,30 @@
+﻿using H2Projekt.Application.Commands;
+using H2Projekt.Application.Interfaces;
+using H2Projekt.Domain;
+
+namespace H2Projekt.Application.Handlers
+{
+    public class DeleteRoomHandler
+    {
+        private readonly IRoomRepository _roomRepository;
+
+        public DeleteRoomHandler(IRoomRepository roomRepository)
+        {
+            _roomRepository = roomRepository;
+        }
+
+        public async Task<bool> Handle(string number, CancellationToken cancellationToken = default)
+        {
+            var room = await _roomRepository.GetRoomByRoomNumberAsync(number, cancellationToken);
+
+            if (room is null)
+            {
+                throw new InvalidOperationException($"Room with number {number} doesn't exist.");
+            }
+
+            var affectedRows = await _roomRepository.DeleteAsync(room, cancellationToken);
+
+            return affectedRows > 0;
+        }
+    }
+}
