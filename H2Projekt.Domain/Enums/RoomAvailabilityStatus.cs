dca2fe699900a2a4e4ab700b219ba3194b1d529b
hillebert1234 <mhillebert@gmail.com>
2026-02-03 10:18:26 +0100

    RoomAvailability added

---
+++ H2Projekt.Domain/Enums/RoomAvailabilityStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H2Projekt.Domain.Enums
+{
+    public enum RoomAvailabilityStatus
+    {
+        Available,
+        Booked,
+        Maintenance
+    }
+}
---
+++ H2Projekt.Domain/RoomAvailability.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using H2Projekt.Domain.Enums;
+
+namespace H2Projekt.Domain
+{
+    public class RoomAvailability
+    {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+        public DateOnly Date { get; set; }
+        public RoomAvailabilityStatus Status { get; set; }
+        public Room Room { get; set; } = default!; 
+        
+        public RoomAvailability(int roomId, DateOnly date, RoomAvailabilityStatus status)
+        {
+            RoomId = roomId;
+            Date = date;
+            Status = status;
+        }
+    }
+}
