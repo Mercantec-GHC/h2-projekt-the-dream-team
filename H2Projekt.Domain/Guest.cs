70409eb0df43b2309eab74b0c4c4c7a03033e41e
hillebert1234 <mhillebert@gmail.com>
2026-02-10 14:48:59 +0100

    ad

--- H2Projekt.Domain/Guest.cs
+++ H2Projekt.Domain/Guest.cs
@@ -5,5 +5,6 @@
         public string FirstName { get; private set; } = default!;
         public string LastName { get; private set; } = default!;
         public string Email { get; private set; } = default!;
+
     }
 }
